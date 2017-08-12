554eed88ad0a59f0e81c7d81e45e69bbaf4eed37
bogdan <peculiarsev@gmail.com>
2017-08-12 17:56:52 +0300

    footer

--- src/js/partials/script.js
+++ src/js/partials/script.js
@@ -5,4 +5,12 @@ $(document).ready(function () {
         nav: true,
         items: 1
     });
+    var t = $(".owl-carousel");
+    t.owlCarousel(),
+        $(".next").click(function () {
+            t.trigger("next.owl.carousel")
+        }),
+        $(".prev").click(function () {
+            t.trigger("prev.owl.carousel")
+        })
 });
